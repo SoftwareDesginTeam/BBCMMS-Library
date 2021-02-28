397a49cc1cc3eedf3416724e6028c3c5d81e5649
Strmec01 <70173190+Strmec01@users.noreply.github.com>
2021-02-27 18:33:09 -0800

    Update Library.js

--- Library.js
+++ Library.js
@@ -1,21 +1,35 @@
 class eBook {
-  constructor(title, authors, genre) {
+  constructor(title, authors, genre) {//Constructs ebook
     this.title = title;
     this.author = author;
     this.genre = genre;
   }
+  edit(x,y,z){
+    this.title = x;
+    this.author = y;
+    this.genre = z;
+    //<script>window.alert(Ebook has been edited);</script> //To be added when consructed in HTML part
+  }
 }
 class Person{
-  constructor(firstName, lastName, email, membershipID, password){
+  constructor(firstName, lastName, email, membershipID, password){//Constructs Person
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.membershipID = memebershipID;
     this.password = password;
   }
+  edit(a,b,c,d,e){
+    this.firstName = a;
+    this.lastName = b;
+    this.email = c;
+    this.membershipID = d;
+    this.password = e;
+    //<script>window.alert(Your Information has been update);</script> //To be added when consructed in HTML part
+  }
 }
   class Account{
-    constructor(memberID, checkedOutEbooks, reservations, active, deleted){
+    constructor(memberID, checkedOutEbooks, reservations, active, deleted){//Constructs Account
       this.memberID = memberID;
       this.checkedOutEbooks = checkedOutEbooks;
       this.reservations =reservations;
