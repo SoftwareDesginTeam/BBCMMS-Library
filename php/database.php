59b604df7c7d9243426168d5ece41a9b8d3856ef
Strmec01 <70173190+Strmec01@users.noreply.github.com>
2021-04-22 15:15:08 -0700

    php

---
+++ php/database.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email']
+    $password = $_POST['password']
+
+
+
+    //Data base values
+    $serverName = "localhost";
+    $userName = "root";
+    $psswrd = "";
+    $dbName = "SQLdata_base";
+
+    //create database connection
+
+    $con = mysql_connect($serverName, $userName, $psswrd, $dbName);
+
+    if(mysql_connect_errno()){
+        echo "Failed to Connect!";
+        exit()
+    }
+    echo "Connection Success!";
+?>
