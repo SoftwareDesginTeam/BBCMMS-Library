bb410ca72e76cbd4bd51ea4399fdfaa4db43e2bf
Strmec01 <70173190+Strmec01@users.noreply.github.com>
2021-04-23 16:23:56 -0700

    php code to add info from register page

--- php/database.php
+++ php/database.php
@@ -3,20 +3,44 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    if (!empty($name) ||!empty($email) || !empty($password)){
+         //create database connection
+         //Data base values
+        $serverName = "localhost";
+        $userName = "root";
+        $psswrd = "";
+        $dbName = "SQLdata_base";
+        $con = mysql_connect($serverName, $userName, $psswrd, $dbName);
 
+        if(mysqli_connect_error()){
+            die('Connect error('.mysqlo_connect_errno().')'.mysqlo_connect_error());
+        }else{
+            $SELECT = "SELECT email From register Where email = ? Limit 1";
+            $INSERT = "INSERT Into register (name, password) values (?,?)";
 
-    //Data base values
-    $serverName = "localhost";
-    $userName = "root";
-    $psswrd = "";
-    $dbName = "SQLdata_base";
+            //Prepare Statement
+            $stmt = $conn->prepare($SELECT);
+            $stmt->bind_param("s",$email);
+            $stmt->execute();
+            $stmt->bind_result($email);
+            $stmt->store_result();
+            $rnum = $stmt->num_rows;
 
-    //create database connection
+            if($rnum == 0){
+                $stmt->close();
 
-    $con = mysql_connect($serverName, $userName, $psswrd, $dbName);
-
-    if(mysql_connect_errno()){
-        echo "Failed to Connect!";
-        exit() }
-    echo "Connection Success!";
+                $stmt = $conn->prepare($INSERT);
+                $stmt->bind_param("ssssii", $name, $email, $password);
+                $stmt->execute();
+                echo "New record instered successfully";
+            }else{
+                echo "Someone already registered using this email";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }else{
+        echo "All Fields Are Required";
+        die();
+    }
 ?>
