dbf82d51a18f63c1a055f8e2034e1b940b0b298a
pbassi3 <70290668+pbassi3@users.noreply.github.com>
2021-04-24 13:10:21 -0700

    Update main.sql

--- sql/main.sql
+++ sql/main.sql
@@ -1,23 +1,29 @@
--- DROP TABLE IF EXISTS "User"
-CREATE TABLE "User"
+CREATE TABLE "UserTable"
 (
     UserId INT NOT NULL PRIMARY KEY, -- primary key column
-    Email TINYTEXT NOT NULL,
-    "Password" TINYTEXT NOT NULL
+    Email VARCHAR NOT NULL,
+    "Password" VARCHAR NOT NULL
     -- specify more columns here
 );
 
 -- DROP TABLE IF EXISTS "BookLendings"
 CREATE TABLE BookLendings
 (
-    UserID INT NOT NULL FOREIGN KEY REFERENCES "User"(UserID),
-    ISBN VARCHAR(25) NOT NULL
+    UserID INT NOT NULL,
+    ISBN VARCHAR(25) NOT NULL,
+	FOREIGN KEY (UserID) REFERENCES "UserTable"(UserID)
 );
 
 -- DROP TABLE IF EXISTS "BookReservations"
 CREATE TABLE BookReservations
 (
-    UserID INT NOT NULL FOREIGN KEY REFERENCES "User"(UserID),
+    UserID INT NOT NULL,
     ISBN VARCHAR(25) NOT NULL,
-    DateReserved DATETIME
+    DateReserved DATE,
+	FOREIGN KEY (UserID) REFERENCES "UserTable"(UserID)
 );
+
+-- As a reference, if needed
+Select * from "UserTable"
+INSERT INTO "UserTable" (UserId, Email, "Password")
+VALUES (15356, 'hatsoff@gmail.com', 'doggy');
